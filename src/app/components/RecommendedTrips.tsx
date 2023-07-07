fd59de3edf0dc0cb26d60ccef2b825d508c9e11f
Eduardo <eduneves95@gmail.com>
2023-07-06 22:40:38 -0300

    feat: add initial trip details page and trip header

--- src/app/components/RecommendedTrips.tsx
+++ src/app/components/RecommendedTrips.tsx
@@ -1,9 +1,16 @@
 import TripItem from "@/components/TripItem";
 import React from "react";
 import { Trip } from '@prisma/client';
+import { prisma } from "@/lib/prisma";
+
+async function getTrips() {
+  const trips = await prisma.trip.findMany({})
+
+  return trips
+}
 
 const RecommendedTrips = async () => {
-  const data = await fetch("http://localhost:3000/hello").then((res) => res.json());
+  const data = await getTrips();
 
   return (
     <div className="container mx-auto p-5">
@@ -18,10 +25,7 @@ const RecommendedTrips = async () => {
           <TripItem key={trip.id} trip={trip} />
         ))}
       </div>
-
     </div>
-
-
   )
 }
 
