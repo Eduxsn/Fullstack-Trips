c2b116b64fe6f53ee48d3ace02b7dfd549ad5332
Eduardo <eduneves95@gmail.com>
2023-07-08 23:47:19 -0300

    feat: add danger variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,15 @@ import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
-  variant?: 'primary' | 'outlined'
+  variant?: 'primary' | 'outlined' | 'danger'
 }
 
 function Button({ className, variant = 'primary', ...props }: ButtonProps) {
 
   const variantClasses = {
     primary: "bg-primary text-white transition-all hover:bg-primaryDarker",
-    outlined: "bg-transparent border-2 border-primary text-primary"
+    outlined: "bg-transparent border-2 border-primary text-primary",
+    danger: "bg-transparent border-2 border-red-500 text-red-500 hover:bg-red-500 hover:text-white",
   }
   const _className = twMerge(
     variantClasses[variant],
