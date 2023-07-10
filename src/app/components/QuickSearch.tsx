777c0a0dd1e892e2d541178a6b6c993d03734b9d
Eduardo <eduneves95@gmail.com>
2023-07-10 15:55:17 -0300

    style: adapt home page to desktop

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -11,26 +11,26 @@ const QuickSearch = () => {
         <div className="w-full h-[1px] bg-grayLighter"></div>
       </div>
 
-      <div className='flex w-full justify-between mt-5'>
+      <div className='flex w-full justify-between mt-5 lg:justify-center lg:gap-40 lg:mt-10'>
         <div className="flex flex-col items-center gap-1 hover:cursor-pointer">
           <Link href= "/trips/search?text=hotel">
             <Image src="/Hotel.svg" alt="" width={50} height={50}/>
           </Link>
-          <p className="text-sm text-grayPrimary">Hotel</p>
+          <p className="text-sm lg:text-base text-grayPrimary">Hotel</p>
         </div>
 
         <div className="flex flex-col items-center gap-1 hover:cursor-pointer">
         <Link href= "/trips/search?text=fazenda">
           <Image src="/Farm.svg" alt="" width={50} height={50}/>
         </Link>
-          <p className="text-sm text-grayPrimary">Fazenda</p>
+          <p className="text-sm lg:text-base text-grayPrimary">Fazenda</p>
         </div>
 
         <div className="flex flex-col items-center gap-1 hover:cursor-pointer">
         <Link href= "/trips/search?text=chalé">
           <Image src="/Cabin.svg" alt="" width={50} height={50}/>
         </Link>
-          <p className="text-sm text-grayPrimary">Chalé</p>
+          <p className="text-sm lg:text-base text-grayPrimary">Chalé</p>
         </div>
 
         <div className="flex flex-col items-center gap-1 hover:cursor-pointer">
