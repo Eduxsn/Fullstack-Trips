bd7648c462b2bdecf5bde785f2a0353c66b6b6be
Eduardo <eduneves95@gmail.com>
2023-07-10 17:15:08 -0300

    style: add margin top

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -24,7 +24,7 @@ const Trips = () => {
   }, [])
 
   return (
-    <div className='container mx-auto flex flex-col items-center lg:items-start p-5'>
+    <div className='container mx-auto flex flex-col items-center lg:items-start p-5 lg:pt-10'>
       <h1 className='text-primaryDarker font-semibold text-xl lg:text-[2.5rem] lg:w-full lg:text-left'>Hospedagens Encontradas</h1>
       <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">{trips.length > 0 ? "Listamos os melhores locais para você!" : "Não encontramos nada nos seus parâmetros =("}</h2>
       <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
